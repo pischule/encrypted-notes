c037c6ba4c65414f7f36b99a8ee9305072b97d3b
Maksim Pischulenok <pischule@outlook.com>
2021-10-05 14:07:24 +0300

    replace notes, fix style

--- src/main/java/bsu/pischule/encryptednotes/service/EncryptionService.java
+++ src/main/java/bsu/pischule/encryptednotes/service/EncryptionService.java
@@ -12,30 +12,22 @@ public class EncryptionService {
         return ("amogus");
     }
 
-    public SessionKeyResponse GenerateData()
-    {
+    public SessionKeyResponse GenerateData() {
         return rsa(generateSessionKey());
     }
 
-    public SessionKeyResponse rsa(String message)
-    {
+    public SessionKeyResponse rsa(String message) {
         //сгенерили ключи
         RSA rsa = new RSA(512);
         rsa.generateKeys();
         var privateKey = rsa.privateKey;
         var publicKey = rsa.publicKey;
         //отправили ключ на сервак
-        {
-
-        }
 
         // на котором зашифровали message
         RSA rsa1 = new RSA(512);
         var enc = rsa1.encrypt(message, publicKey);
         //отправили зашифрованный текст клиенту
-        {
-
-        }
 
         //расшифровали на клиенте
         RSA rsa2 = new RSA(512);
@@ -44,8 +36,7 @@ public class EncryptionService {
         return new SessionKeyResponse(res, rsa.cipherToString(enc), publicKey.toString(), privateKey.toString());
     }
 
-    public EncryptedNoteResponse cfb(String message, String sessionKey)
-    {
+    public EncryptedNoteResponse cfb(String message, String sessionKey) {
         sessionKey = sessionKey.replaceAll("[^a-zA-Z0-9]", "");
         var enc = RealCFB.encrypt(message, sessionKey);
         var dec = RealCFB.decrypt(enc, sessionKey);
